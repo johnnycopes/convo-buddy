0f18f2e5861874f60f18e421e9f9b9d6875ab285
John Coppola <jdcoppola@gmail.com>
2016-12-19 19:14:39 -0500

    basic layout up, about to crack into back-end requests

--- schema.js
+++ schema.js
@@ -34,3 +34,89 @@ exports.Category = mongoose.model('Category', categorySchema);
 exports.Question = mongoose.model('Question', questionSchema);
 exports.User = mongoose.model('User', userSchema);
 exports.Token = mongoose.model('Token', tokenSchema);
+
+
+
+
+
+// Dummy data setup
+// =====================
+
+// Category.create({
+//   name: 'hypothetical',
+//   userId: null, // if the category is modified, attach this userId to this field
+//   parentId: null // if the category is modified, attach this category's objectId to this field
+// });
+
+// Question.create({
+//   text: 'What change do you want in your life within the next five years?',
+//   categories: [{
+//     name: 'future tense',
+//     _id: ObjectId("585837d0e247fe9e25b8180a")
+// }],
+//   numLikes: 0,
+//   userId: null, // if the question is modified, attach this userId to this field
+//   parentId: null, // if the question is modified, attach this question's objectId to this field
+//   isLiked: false
+// })
+//   .then((results) => {
+//     console.log('success');
+//     console.log(results);
+//   })
+//   .catch((err) => {
+//     console.log('error');
+//     console.log(err.message);
+//     console.log(err.errors);
+//   });
+
+
+// Test queries
+// =====================
+
+// 1) get all questions from the db, period
+
+// Question.find({})
+//   .then((results) => {
+//     console.log('found all questions');
+//     console.log(results);
+//   })
+//   .catch((err) => {
+//     console.log('failed');
+//     console.log(err.message);
+//   });
+
+// 2) get all questions from a specific category
+
+// let category = 'past tense';
+// Question.find({
+//     $and: [
+//         { 'categories.name': 'future tense' },
+//         { 'categories.name': 'hypothetical' }
+//     ]
+// })
+//   .then((results) => {
+//     console.log('found questions from one category');
+//     console.log(results);
+//   })
+//   .catch((err) => {
+//     console.log('failed');
+//     console.log(err.message);
+//   });
+
+// 3) get all questions from multiple categories
+
+// let category = 'past tense';
+// Question.find({
+//     $and: [
+//         { 'categories.name': 'future tense' },
+//         { 'categories.name': 'hypothetical' }
+//     ]
+// })
+//   .then((results) => {
+//     console.log('found questions from multiple categories');
+//     console.log(results);
+//   })
+//   .catch((err) => {
+//     console.log('failed');
+//     console.log(err.message);
+//   });
